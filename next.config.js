c059ef0f8547ff2259808e043dd1991e8c124e86
omerber242 <bbar@parallelwireless.com>
2026-06-06 15:25:03 +0300

    new

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
 const nextConfig = {
   images: {
     domains: [],
   },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
